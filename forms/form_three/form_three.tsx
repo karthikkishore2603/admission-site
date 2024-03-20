94c0890724d97375dd2c55de3dbffbe0c30a15a5
Lakshay1406 <142895484+Lakshay1406@users.noreply.github.com>
2024-03-20 09:18:39 +0530

    second form updated

--- forms/form_three/form_three.tsx
+++ forms/form_three/form_three.tsx
@@ -52,35 +52,154 @@ export default function Formthree() {
             paddingBottom: "20px",
           }}
         />
+        <TextField
+          id="outlined-basic"
+          label="Address for communication"
+          variant="outlined"
+          name="address for communication"
+          sx={{
+            width: "70%",
+            paddingBottom: "20px",
+          }}
+        />
 
-<FormControl
+        <TextField
+          id="outlined-basic"
+          label="Mail-id"
+          variant="outlined"
+          name="mail-id"
+          sx={{
+            width: "70%",
+            paddingBottom: "20px",
+          }}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Phone number"
+          variant="outlined"
+          name="phone number"
+          sx={{
+            width: "70%",
+            paddingBottom: "20px",
+          }}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Date of birth"
+          variant="outlined"
+          name="dob"
+          sx={{
+            width: "70%",
+            paddingBottom: "20px",
+          }}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Name of school studied"
+          variant="outlined"
+          name="school name"
+          sx={{
+            width: "70%",
+            paddingBottom: "20px",
+          }}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="School code No."
+          variant="outlined"
+          name="school code"
+          sx={{
+            width: "70%",
+            paddingBottom: "20px",
+          }}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="12th Exam registration No."
+          variant="outlined"
+          name="12th reg no."
+          sx={{
+            width: "70%",
+            paddingBottom: "20px",
+          }}
+        />
+
+        <FormControl
             sx={{
               width: "70%",
               marginBottom: "20px",
             }}
           >
-            <InputLabel>Enquiry Through</InputLabel>
+            <InputLabel>Category</InputLabel>
             <Select
-              labelId="demo-simple-select-label"
+              labelId="demo-simple-select-label1"
               id="demo-simple-select"
-              label="Enquiry Through"
-              name="enquirythrough"
+              label="category"
+              value={formData.category}
+              onChange={handleChange}
+              name="category"
               sx={{
                 width: "100%",
                 marginBottom: "20px",
               }}
             >
-              <MenuItem value="parents">Parents</MenuItem>
-              <MenuItem value="sibilings">Sibilings</MenuItem>
-              <MenuItem value="relativeandfriends">
-                Relative And Friends
-              </MenuItem>
-              <MenuItem value="alumini">Alumini</MenuItem>
-              <MenuItem value="collegewebsite">College Website</MenuItem>
-              <MenuItem value="socialmedia">Social Media</MenuItem>
+              <MenuItem value="bc">BC</MenuItem>
+              <MenuItem value="bcm">BCM</MenuItem>
+              <MenuItem value="mbc">MBC</MenuItem>
+              <MenuItem value="bcm">BCM</MenuItem>
+              <MenuItem value="sca">SCA</MenuItem>
+              <MenuItem value="sc">SC</MenuItem>
+              <MenuItem value="st">ST</MenuItem>
+              <MenuItem value="oc">OC</MenuItem>
               <MenuItem value="other">Other</MenuItem>
             </Select>
-           </FormControl>
+            {category === "other" && (
+              <TextField
+                sx={{
+                  width: "100%",
+                  paddingBottom: "20px",
+                }}
+                label="Enter Other Value"
+                value={formData.category}
+                onChange={handleChange}
+                name="category"
+              />
+            )}
+        </FormControl>
+
+        <FormControl
+            sx={{
+              width: "70%",
+              marginBottom: "20px",
+            }}
+          >
+            <InputLabel>Willing to avail merit scholarship?</InputLabel>
+            <Select
+              labelId="scholarship"
+              id="scholarship"
+              label="scholarship"
+              value={formData.category}
+              onChange={handleChange}
+              name="scholarship"
+              sx={{
+                width: "100%",
+                marginBottom: "20px",
+              }}
+            >
+              <MenuItem value="yes">Yes</MenuItem>
+              <MenuItem value="no">No</MenuItem>
+            </Select>
+        </FormControl>  
+
+        <Button variant="contained" onClick={handleFormSubmit}>
+            Submit
+        </Button>
+
 
       </Box>
     </>
